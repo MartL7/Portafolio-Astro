e34a9a6560bd1be5db0720ea180a31d5515ca138
MartL7 <latiapelitos123@gmail.com>
2024-02-12 02:40:01 -0600

    update Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://martl7.github.io/Port-Folio-Astro/",
+  site: "martl7.github.io/Port-Folio-Astro",
   base: "/Port-Folio-Astro/",
 });

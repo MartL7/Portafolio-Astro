3aec54724781978d84cb3d28fb6ad33f76dd1ca8
MartL7 <latiapelitos123@gmail.com>
2024-02-12 12:42:34 -0600

    Updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://martl7.github.io/Port-Folio-Astro",
+  output: "server",
+  adapter: vercel()
 });
